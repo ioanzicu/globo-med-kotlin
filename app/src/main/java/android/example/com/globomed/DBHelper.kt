cb915a22e260a337e673fa48db7b9271f458f47a
ioanz <ioan.zicu94@gmail.com>
2020-01-26 21:13:05 +0100

    create database table

--- app/src/main/java/android/example/com/globomed/DBHelper.kt
+++ app/src/main/java/android/example/com/globomed/DBHelper.kt
@@ -5,12 +5,25 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class DBHelper(context: Context?) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    // called when the database is created for the first time
     override fun onCreate(db: SQLiteDatabase?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        db?.execSQL(GloboMedDBContract.SQL_CREATE_ENTRIES)
     }
 
+    // called when the schema or database version changed
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        db?.execSQL(GloboMedDBContract.SQL_DROP_TABLE)
+        onCreate(db)
+    }
+
+    // called when we need to downgrade the downgrade the database, when old version is newer than the new database
+    override fun onDowngrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        super.onDowngrade(db, oldVersion, newVersion)
+    }
+
+    // triggered when database schema was created, upgraded or downgraded
+    override fun onOpen(db: SQLiteDatabase?) {
+        super.onOpen(db)
     }
 
     companion object {
--- app/src/main/java/android/example/com/globomed/GloboMedDBContract.kt
+++ app/src/main/java/android/example/com/globomed/GloboMedDBContract.kt
@@ -3,6 +3,16 @@ package android.example.com.globomed
 import android.provider.BaseColumns
 
 object GloboMedDBContract {
+
+    const val SQL_CREATE_ENTRIES: String =
+        "CREATE TABLE ${EmployeeEntry.TABLE_NAME} {" +
+                "${EmployeeEntry.COLUMN_ID} INTEGER PRIMARY KEY AUTO INCREMENT, " +
+                "${EmployeeEntry.COLUMN_NAME} TEXT NOT NULL, " +
+                "${EmployeeEntry.COLUMN_DOB} INTEGER, NOT NULL" +
+                "${EmployeeEntry.COLUMN_DESIGNATION} TEXT NOT NULL"
+
+    const val SQL_DROP_TABLE = "DROP TABLE IF EXISTS ${EmployeeEntry.TABLE_NAME}"
+
     object EmployeeEntry : BaseColumns {
         const val TABLE_NAME = "employee"
         const val COLUMN_ID = "id"
