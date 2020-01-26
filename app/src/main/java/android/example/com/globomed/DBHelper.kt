fc1d0cf9144d4c6e8bfd6ff58614c609f4446e1d
ioanz <ioan.zicu94@gmail.com>
2020-01-26 20:56:37 +0100

    subclass for SQLiteOpenHelper

---
+++ app/src/main/java/android/example/com/globomed/DBHelper.kt
@@ -0,0 +1,20 @@
+package android.example.com.globomed
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHelper(context: Context?) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    companion object {
+        const val DATABASE_NAME = "globomed.db"
+        const val DATABASE_VERSION = 1
+    }
+}
